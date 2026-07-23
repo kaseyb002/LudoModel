8e76c8ff5c9559e9e4dde3a7f867ef8cc91ed7ea
Kasey Baughan <kaseyb002@yahoo.com>
2026-07-23 11:53:14 -0700

    Lower swift-tools-version to 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,6 +22,5 @@ let package = Package(
             name: "LudoModelTests",
             dependencies: ["LudoModel"]
         ),
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
